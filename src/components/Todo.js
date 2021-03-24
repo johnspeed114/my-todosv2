d73c55d7055dc84f99eacd98b661bf8ae0353fc5
johnspeed114 <johnlaw114@gmail.com>
2021-03-24 20:45:35 +0900

    Added Strikethrough

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,15 +1,18 @@
 import React from 'react';
+import './../App.css'
 
 function Todo({handleRemove, item, handleComplete}) {
     return (
-    <li>
+    <li key= {item.id} className={(item.completed)? 'done': ''}>
         <input type="checkbox" onClick={()=>handleComplete(item.id)}/>
         {item.name}
+        
         <button type="button" onClick={() => handleRemove(item.id)}>
             delete
         </button>
     </li>
     )
 }
+
 
 export default Todo;
